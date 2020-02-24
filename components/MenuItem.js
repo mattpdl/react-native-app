423f8b168ad3740f944f95ac69efb7bbb31994f6
Matthew Ponce de Leon <mattpdl01@gmail.com>
2020-02-23 17:41:40 -0800

    Add MenuItem component

---
+++ components/MenuItem.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+    menuItem: {
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        margin: 10,
+    },
+
+    image: {
+        flex: 3,
+        height: 300,
+        width: 400,
+    },
+
+    name: {
+        flex: 1,
+    },
+
+    text: {
+        fontSize: 16,
+    },
+});
+
+export default function MenuItem(props) {
+    return (
+        <View style={styles.menuItem}>
+            <Image source={require(props.img)} style={styles.image}></Image>
+            <View style={styles.name}>
+                <Text style={styles.text}>{props.name}</Text>
+            </View>
+        </View>
+    );
+}
--- screens/MenuScreen.js
+++ screens/MenuScreen.js
@@ -1,15 +1,14 @@
 import * as React from 'react';
 import { ScrollView, Text } from 'react-native';
 
+
+
 export default function MenuScreen() {
     const [menu, setMenu] = React.useState();
     React.useEffect(() => {
-        fetch("http://44.232.86.238/dining/menu/detailedMenu")
+        fetch("http://44.232.86.238/dining/menu/overviewMenu")
             .then(resp => resp.json())
-            .then(data => {
-                console.log(data);
-                setMenu(data);
-            });
+            .then(data => setMenu(data));
     }, [])
     return <ScrollView><Text>{JSON.stringify(menu, null, 2)}</Text></ScrollView>;
 }
